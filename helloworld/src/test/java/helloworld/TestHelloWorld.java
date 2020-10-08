d01ace2760a16a5a88ca5d1cab0c975667c3f0dc
srinath <sriv5@DESKTOP-RI1PC0O.home>
2020-10-08 01:32:13 -0400

    maven test commit

---
+++ helloworld/src/test/java/helloworld/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testhelloworld() {
+
+		System.out.println("--------------");
+		System.out.println("Test Helloooo");
+		System.out.println("--------------");
+	}
+
+}
